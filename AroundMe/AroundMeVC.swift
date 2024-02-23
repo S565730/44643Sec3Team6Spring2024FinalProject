0c4e37af84eba491c5c45cdefce08a34f41c09ca
Dhruvitha Challa <challadhruvitha08@gmail.com>
2024-02-23 02:25:23 -0600

    Dhruvitha : Added lottie asset

--- AroundMe/AroundMeVC.swift
+++ AroundMe/AroundMeVC.swift
@@ -15,9 +15,30 @@ class AroundMeVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var ContentView: UIView!
+    
+   
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    @IBOutlet weak var CoffeBTN: UIButton!
+    
+    
+    @IBOutlet weak var HospitalBTN: UIButton!
+    
+    
+    @IBOutlet weak var BankBTN: UIButton!
+    
+    
+    @IBOutlet weak var RestaurantBTN: UIButton!
+    
+    
+    @IBOutlet weak var MovieBTN: UIButton!
+    
+    @IBOutlet weak var ShoppingBTN: UIButton!
     /*
-    // MARK: - Navigation
+     @IBOutlet weak var GasStationBTN: UIButton!
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
