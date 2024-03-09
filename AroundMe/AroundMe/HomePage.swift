80578b05d63f5bf8a477ae998ecd20b3dd0b346a
Dhruvitha Challa <challadhruvitha08@gmail.com>
2024-03-08 18:57:54 -0600

    Dhruvitha-3Commit--Created a new HomeVC and created outlets for login and password Text fields and for Go Button and also Action for the Button.

---
+++ AroundMe/AroundMe/HomePage.swift
@@ -0,0 +1,38 @@
+//
+//  HomePage.swift
+//  AroundMe
+//
+//  Created by Challa Dhruvitha on 3/8/24.
+//
+
+import UIKit
+
+class HomePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBOutlet weak var UserNameTF: UITextField!
+    
+    @IBAction func GoBTN(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var PasswordTF: UITextField!
+    
+    @IBOutlet weak var GoTF: UIButton!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
