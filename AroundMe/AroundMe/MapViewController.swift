1e26049c891f5cf780ed81045bfae4cf7f5d22c0
Mahitha <s566410@nwmissouri.edu>
2024-04-12 10:31:14 -0500

    Mahitha : added mapview controller and related tools

---
+++ AroundMe/AroundMe/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  AroundMe
+//
+//  Created by MAHITHA VUDUTHA on 4/12/24.
+//
+
+import UIKit
+import MapKit
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
